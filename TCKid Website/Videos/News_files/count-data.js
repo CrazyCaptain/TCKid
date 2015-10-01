c0443b36b9017d6f7d5f23f03e2b6ab2383914de
DrSpaceInvader <masahud.ahmed@hotmail.com>
2015-10-01 12:56:36 +0800

    Initial Upload
    TcKid Website

---
+++ TCKid Website/Videos/News_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"0 Comments","multiple":"{num} Comments","one":"1 Comment"}},"counts":[{"id":"358 http:\/\/news.tckid.com\/?p=358","comments":19},{"id":"304 http:\/\/news.tckid.com\/?p=304","comments":2},{"id":"348 http:\/\/news.tckid.com\/?p=348","comments":0},{"id":"288 http:\/\/news.tckid.com\/?p=288","comments":0},{"id":"313 http:\/\/news.tckid.com\/?p=313","comments":5},{"id":"366 http:\/\/news.tckid.com\/?p=366","comments":4},{"id":"269 http:\/\/news.tckid.com\/?p=269","comments":0},{"id":"353 http:\/\/news.tckid.com\/?p=353","comments":2},{"id":"274 http:\/\/news.tckid.com\/?p=274","comments":0},{"id":"278 http:\/\/news.tckid.com\/?p=278","comments":0}]});
+}
